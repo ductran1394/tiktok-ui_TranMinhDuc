6c2bbaaff86d87764470ad6bbf850d52e580c8e6
ductran1394 <ductran1394@gmail.com>
2022-07-04 18:16:32 +0700

    Cấu hình Router/Layout cho dự án

---
+++ src/pages/Upload/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import Header from "~/components/Layout/DefaultLayout/Header";
+
+export default function Upload() {
+   return (
+      <div>
+         <h2>Upload page</h2>
+      </div>
+   );
+}
---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+import {HeaderOnly} from "~/components/Layout";
+import Following from "~/pages/Following/Following";
+import Home from "~/pages/Home/index";
+import Search from "~/pages/Search";
+import Upload from "~/pages/Upload/index";
+
+const publicRoutes = [
+   {path: "/", component: Home},
+   {path: "/following", component: Following},
+   {path: "/upload", component: Upload, layout: HeaderOnly},
+   {path: "/search", component: Search, layout: null},
+];
+const privateRoutes = [];
+
+export {publicRoutes, privateRoutes};
