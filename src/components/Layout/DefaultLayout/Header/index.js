d37c10c71475ba8b73b9db3d8a3ced4fabf5bf89
ductran1394 <ductran1394@gmail.com>
2022-07-11 17:49:39 +0700

    Xây Dựng UI Phần Header Dự Án Tiktok #5

--- src/components/Layout/DefaultLayout/Header/index.js
+++ src/components/Layout/DefaultLayout/Header/index.js
@@ -25,6 +25,21 @@ const MENU_ITEMS = [
    {
       icon: <FontAwesomeIcon icon={faEarthAsia} />,
       title: "English",
+      children: {
+         title: "Language",
+         data: [
+            {
+               type: "language",
+               code: "en",
+               title: "English",
+            },
+            {
+               type: "language",
+               code: "vi",
+               title: "Tiếng Việt",
+            },
+         ],
+      },
    },
    {
       icon: <FontAwesomeIcon icon={faCircleQuestion} />,
@@ -48,6 +63,17 @@ export default function Header() {
    //    []);
 
    const searchResult = [];
+
+   // Handle logic
+   const handleMenuChange = (menuItem) => {
+      // switch (menuItem.type) {
+      //    case "language":
+      //       // Handle change language
+      //       break;
+      //    default:
+      // }
+      console.log(menuItem);
+   };
 
    return (
       <header className={cx("wrapper")}>
@@ -85,7 +111,7 @@ export default function Header() {
                <Button text>Upload</Button>
                <Button primary>Log in</Button>
 
-               <Menu items={MENU_ITEMS}>
+               <Menu items={MENU_ITEMS} onChange={handleMenuChange}>
                   <button className={cx("more-btn")}>
                      <FontAwesomeIcon icon={faEllipsisVertical} />
                   </button>
