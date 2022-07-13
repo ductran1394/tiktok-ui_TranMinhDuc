77e6b72e10cb7528041e39aa84c310999b9d6c9d
ductran1394 <ductran1394@gmail.com>
2022-07-13 11:21:21 +0700

    Xây Dựng Logic Phần Header Dự Án Tiktok #2

--- src/routes/index.js
+++ src/routes/index.js
@@ -8,6 +8,7 @@ const publicRoutes = [
    {path: "/", component: Home},
    {path: "/following", component: Following},
    {path: "/upload", component: Upload, layout: HeaderOnly},
+   // {path: "/@:nickname", component: Profile},
    {path: "/search", component: Search, layout: null},
 ];
 const privateRoutes = [];
